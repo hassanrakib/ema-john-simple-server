7a3dc74acf68bd490094968e2743eaf9925cee35
Rakib Hassan <mailrakibhassan@gmail.com>
2023-02-19 16:24:56 +0600

    post request to fix cart

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -15,25 +15,36 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri);
 
 async function run() {
-    try {
-        // collections
-        const products = client.db("emaJohn").collection("products");
+  try {
+    // collections
+    const products = client.db("emaJohn").collection("products");
 
-        // get products
-        app.get("/products", async (req, res) => {
-            const currentPageIndex = req.query.page;
-            const productsPerPage = req.query.size;
-            console.log(currentPageIndex, productsPerPage);
-            const query = {};
-            const cursor = products.find(query);
-            const result = await cursor.toArray();
-            const count = await products.estimatedDocumentCount();
-            res.send({count, result});
-        })
-    }
-    finally {
+    // get products
+    app.get("/products", async (req, res) => {
+      const currentPageIndex = parseInt(req.query.page);
+      const productsPerPage = parseInt(req.query.size);
+      const query = {};
+      const cursor = products.find(query);
+      const result = await cursor
+        .skip(currentPageIndex * productsPerPage)
+        .limit(productsPerPage)
+        .toArray();
+      const count = await products.estimatedDocumentCount();
+      res.send({ count, result });
+    });
 
-    }
+    // get products by their _id property
+    // post method is used to send ids from the front end
+    app.post("/productsByIds", async (req, res) => {
+      const ids = req.body;
+      const idsWithObjectId = ids.map(id => new ObjectId(id));
+      const query = {_id: {$in : idsWithObjectId}};
+      const cursor = products.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+  }
 }
 
 run().catch(console.dir);
